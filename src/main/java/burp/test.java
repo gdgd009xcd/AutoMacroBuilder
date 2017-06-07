06ca543a85727d7c5c028238c9d66a8afbb70efe
chikara_1.daike <chikara_1.daike@TSS-P001031.dir.toppan.co.jp>
2017-06-07 17:17:53 +0900

    indexOf対応

--- src/main/java/burp/test.java
+++ src/main/java/burp/test.java
@@ -13,41 +13,37 @@ public class test {
     public static void main(String[] args){
         //ParmGenCSV(String _filename, String _lang)
         ParmVars.parmfile = "xxx.csv";
-        String seq = new String("1ABABABCxD123ABCD4");
-        String key = new String("ABCx");
+        String seq = new String("AAAABBABABCxD123ABCD4");
+        String key = new String("AAAB");
 
         byte[] seqbin = seq.getBytes();
         byte[] keybin = key.getBytes();
 
-        int startpos = 2;
+        int startpos = 0;
+        int endpos = seqbin.length - keybin.length +1;
 
 
         System.out.println("String.indexof=" + seq.indexOf(key, startpos));
 
-        	int j = 0;
-        	int idx = 0;
-        	boolean found = false;
-        	for(int i = startpos; i<seqbin.length; i++){
-        		System.out.println("  i,j="  + i + "," + j);
-        		if(seqbin[i] != keybin[j]){
-        			j = 0;
+        	int idx = -1;
+        	for(int i = startpos; i<endpos; i++){
+        		for(int j=0; j<keybin.length; j++){
+	        		System.out.println("  i,j="  + i + "," + j);
+	        		
+	        		if(seqbin[i+j] == keybin[j]){
+	        			if(j == keybin.length -1 ){
+	        				idx = i;
+	        				System.out.println(" result idx,i,j=" + idx+ "," + i + "," + j);
+	        				break;
+	        			}
+
+	        		}else{
+	        			break;
+	        		}
         		}
-        		if(seqbin[i] == keybin[j]){
-        			if(j == 0){
-        				idx = i;
-        			}
-        			if(++j == keybin.length ){
-        				found = true;
-        				System.out.println(" result idx,i,j=" + idx+ "," + i + "," + j);
-        				break;
-        			}
-        		}else{
-        			j = 0;
-        		}
+        		if(idx!=-1)break;
         	}
-        	if(!found){
-        		idx = -1;
-        	}
+        	
         	System.out.println(" result idx=" + idx);
     }
 

2c409ebee44aabc216b1c95bec2b6aaae0725c93
youtube <youtube@192.168.10.102>
2017-06-19 00:49:09 +0900

    追跡直前以前のリクエスト対応

---
+++ src/main/java/burp/ParmGenResToken.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package burp;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author youtube
+ */
+public class ParmGenResToken {
+    public ArrayList<ParmGenToken> requesttokenlist;
+    public PRequest request;  
+}
